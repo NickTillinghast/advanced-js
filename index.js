4ace760f0091004395190303b87e75125b7457a4
Nick Tillinghast <nickt2@cox.net>
2020-06-01 20:47:11 -0700

    refer to repl

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 //arity
 
-// fn1(fn2(fn3(50)));
-// compose(fn1, fn2, fn3)(50);
-// pipe(fn3, fn2, fn1)(50);
-//compose
-//pipe
+fn1(fn2(fn3(50)));
+compose(fn1, fn2, fn3)(50);
+pipe(fn3, fn2, fn1)(50);
+compose
+pipe
 
-// const compose = (f, g) => (data) => f(g(data));
-// const pipe = (f, g) => (data) => g(f(data));
-// const multipyBy3 = (num) => num * 3;
-// const makePositive = (num) => Math.abs(num);
-// const multiplyBy3AndAbsolute = compose(multipyBy3, makePositive);
+const compose = (f, g) => (data) => f(g(data));
+const pipe = (f, g) => (data) => g(f(data));
+const multipyBy3 = (num) => num * 3;
+const makePositive = (num) => Math.abs(num);
+const multiplyBy3AndAbsolute = compose(multipyBy3, makePositive);
 
 
 // Amazon shopping
